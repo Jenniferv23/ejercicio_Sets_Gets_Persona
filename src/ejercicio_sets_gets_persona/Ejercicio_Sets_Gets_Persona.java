f15be6da649298870e560457ce84b3592d6e5b25
Jenniferv23 <jvanegas369@misena.edu.co>
2022-09-08 18:36:34 -0500

    Se agrega el ejercicio

---
+++ src/ejercicio_sets_gets_persona/Ejercicio_Sets_Gets_Persona.java
@@ -0,0 +1,53 @@
+
+package ejercicio_sets_gets_persona;
+
+public class Ejercicio_Sets_Gets_Persona {
+
+    public static void main(String[] args) {
+        
+        Persona persona = new Persona ();
+        
+        persona.setEdad(33);
+        persona.setNombre ("Jennifer Vanegas");
+        persona.setTelefono (3127);
+        
+        
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+        
+    }
+    
+}
+
+class Persona {
+    
+    private int edad;
+    private String nombre;
+    private int telefono;
+    
+    public void setEdad(int edad){
+            this.edad = edad;
+    }
+    
+    public int getEdad () {
+            return this.edad;
+    }
+    
+    public void setNombre(String nombre){
+            this.nombre = nombre;
+    }
+    
+    public String getNombre () {
+            return this.nombre;
+    }
+    
+    public void setTelefono(int telefono){
+            this.telefono = telefono;
+    }
+    
+    public int getTelefono () {
+            return this.telefono;
+    }
+    
+}
